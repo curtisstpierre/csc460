38e36e743ed2e0c9bec2cd4439aaf4e79352ae1c
MarkRoller <eng.mroller@gmail.com>
2015-03-30 01:49:16 -0700

    Initial commit of project including the addition of our past code

---
+++ project3/project3/c_buffer.h
@@ -0,0 +1,25 @@
+/*
+ * c_buffer.h
+ *
+ *  Created: May 28, 2013
+ *  Author: andpol
+ */
+
+#ifndef C_BUFFER_H_
+#define C_BUFFER_H_
+
+#include <stdlib.h>
+
+typedef struct {
+	uint8_t *data;
+	uint8_t start;
+	uint8_t len;
+} cirBuffer_t;
+
+void init_cirBuffer(cirBuffer_t *buffer, size_t size){
+	buffer->len = 0;
+	buffer->start = 0;
+	buffer->data = calloc(size, 1);
+}
+
+#endif /* C_BUFFER_H_ */
